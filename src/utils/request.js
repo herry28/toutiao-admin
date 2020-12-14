c63a6053269d6d461cfc8d83a595ad9385bb0d47
zouhai <zouhai_@outlook.com>
2020-12-14 16:26:28 +0800

    '项目优化'

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,8 @@
  */
 import axios from 'axios'
 import JSONbig from 'json-bigint'
+import router from '@/router/router.js'
+import {Message} from 'element-ui'
 
 
 
@@ -28,7 +30,24 @@ request.interceptors.request.use((config)=>{
     }
     return config //config是当前请求的配置对象，只有return config之后请求才能真的发送出去
 })
+// 响应拦截器
+request.interceptors.response.use((response)=>{//所有响应为2xx都会进入这里
 
+    return response
+},(error)=>{//所有响应不是2xx都会进入这里
+    if(error.response && error.response.status===401){
+        // 清除本地存储中的用户登录状态
+        window.localStorage.removeItem('user')
+        // 跳转到登录页面
+        router.push('/login')
+        Message('登录状态无效，请重新登录')
+    }else if(error.response.status===403){//没有操作权限
+        Message('没有操作权限')
+    }else if(error.response.status>=500){//服务端错误
+        Message('服务端错误，请稍后重试')
+    }
+    return Promise.reject(error)
+})
 
 // 导出请求方法
 export default request
